6c87609ef7edceda870a44e493bd879fe4c65386
zimoadmin <141346171+zimoadmin@users.noreply.github.com>
2026-08-13 19:05:37 +0800

    Update github.sh

--- lib/github.sh
+++ lib/github.sh
@@ -4,23 +4,10 @@ OPENCLASH_API="https://api.github.com/repos/vernesong/OpenClash/releases/latest"
 
 get_latest_release() {
 
-    info "Getting latest OpenClash release..."
+    info "github.sh loaded"
 
-    JSON="$(wget -qO- "$OPENCLASH_API")"
+    JSON="$(wget -qO- https://api.github.com/repos/vernesong/OpenClash/releases/latest)"
 
-    if [ -z "$JSON" ]; then
-        error "Failed to connect GitHub API"
-        exit 1
-    fi
+    echo "$JSON" | head -n 5
 
-    RELEASE_TAG="$(echo "$JSON" | grep '"tag_name"' | head -n1 | cut -d '"' -f4)"
-
-    DOWNLOAD_URL="$(echo "$JSON" \
-        | grep browser_download_url \
-        | grep "luci-app-openclash" \
-        | cut -d '"' -f4 \
-        | head -n1)"
-
-    info "Latest Version : $RELEASE_TAG"
-    info "Download URL : $DOWNLOAD_URL"
 }
