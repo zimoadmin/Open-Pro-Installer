392773bd8075f466e7f3447dc5c27965bc33ce48
zimoadmin <141346171+zimoadmin@users.noreply.github.com>
2026-08-13 19:30:29 +0800

    Update github.sh

--- lib/github.sh
+++ lib/github.sh
@@ -6,17 +6,33 @@ get_latest_release() {
 
     info "Getting latest OpenClash release..."
 
-    wget -qO /tmp/openclash_version "$OPENCLASH_API"
-
-    [ ! -f /tmp/openclash_version ] && {
-        error "Download release info failed"
+    # 下载 Release 信息
+    if ! wget -qO /tmp/openclash_version "$OPENCLASH_API"; then
+        error "Failed to download release information."
         return 1
-    }
+    fi
 
+    # 获取版本号
     RELEASE_TAG="$(jsonfilter -i /tmp/openclash_version -e '@.tag_name')"
 
-    DOWNLOAD_URL="$(jsonfilter -i /tmp/openclash_version -e '@.assets[*].browser_download_url' | grep '\.ipk$' | head -n1)"
+    # 自动判断包管理器
+    if command -v apk >/dev/null 2>&1; then
+        PACKAGE_EXT="apk"
+    elif command -v opkg >/dev/null 2>&1; then
+        PACKAGE_EXT="ipk"
+    else
+        error "Unsupported package manager."
+        return 1
+    fi
+
+    # 获取对应下载地址
+    DOWNLOAD_URL="$(
+        jsonfilter -i /tmp/openclash_version -e '@.assets[*].browser_download_url' \
+        | grep "\.${PACKAGE_EXT}$" \
+        | head -n1
+    )"
 
     info "Latest Version : $RELEASE_TAG"
-    info "Download URL : $DOWNLOAD_URL"
+    info "Package Type   : $PACKAGE_EXT"
+    info "Download URL   : $DOWNLOAD_URL"
 }
