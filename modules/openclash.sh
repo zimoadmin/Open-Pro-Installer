a3449820e7094ca1dd5911d08dbc76b470639f91
zimoadmin <141346171+zimoadmin@users.noreply.github.com>
2026-08-13 19:35:09 +0800

    Update openclash.sh

--- modules/openclash.sh
+++ modules/openclash.sh
@@ -1,7 +1,40 @@
 #!/bin/sh
 
 install_openclash() {
+
     info "Ready to install OpenClash"
     info "Version : $RELEASE_TAG"
     info "URL : $DOWNLOAD_URL"
+
+    PKG_FILE="/tmp/openclash.${PACKAGE_EXT}"
+
+    info "Downloading..."
+
+    if ! wget -O "$PKG_FILE" "$DOWNLOAD_URL"; then
+        error "Download failed!"
+        return 1
+    fi
+
+    info "Download complete."
+
+    ls -lh "$PKG_FILE"
+
+    info "Installing..."
+
+    if [ "$PACKAGE_EXT" = "apk" ]; then
+        apk add \
+            --allow-untrusted \
+            --force-overwrite \
+            --clean-protected \
+            "$PKG_FILE"
+    else
+        opkg install "$PKG_FILE"
+    fi
+
+    if [ $? -eq 0 ]; then
+        info "OpenClash installed successfully!"
+    else
+        error "OpenClash installation failed!"
+        return 1
+    fi
 }
