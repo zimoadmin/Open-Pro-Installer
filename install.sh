1799552ad196cb8608476dfab68ad7ea71f7b7a0
zimoadmin <141346171+zimoadmin@users.noreply.github.com>
2026-08-13 19:18:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
+#!/bin/sh
+
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+
+. "$SCRIPT_DIR/lib/logger.sh"
+. "$SCRIPT_DIR/lib/github.sh"
+
+clear
+
+echo "======================================"
+echo "        Open-Pro-Installer"
+echo "======================================"
+
+echo "1. Install OpenClash"
+echo "0. Exit"
+
+printf "Choose: "
+read CHOOSE
+
 case "$CHOOSE" in
 1)
     echo "===== STEP 1 ====="
@@ -13,5 +32,8 @@ case "$CHOOSE" in
     install_openclash
 
     echo "===== STEP 4 ====="
+    ;;
+*)
+    exit 0
     ;;
 esac
