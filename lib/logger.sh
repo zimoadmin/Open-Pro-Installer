92d59b8dea7ac8db6f7dab67c7bebc666211e923
zimoadmin <141346171+zimoadmin@users.noreply.github.com>
2026-08-13 18:47:54 +0800

    Update logger.sh

--- lib/logger.sh
+++ lib/logger.sh
@@ -1,1 +1,18 @@
 #!/bin/sh
+
+GREEN="\033[32m"
+RED="\033[31m"
+YELLOW="\033[33m"
+RESET="\033[0m"
+
+info() {
+    echo "${GREEN}[INFO]${RESET} $1"
+}
+
+warn() {
+    echo "${YELLOW}[WARN]${RESET} $1"
+}
+
+error() {
+    echo "${RED}[ERROR]${RESET} $1"
+}
