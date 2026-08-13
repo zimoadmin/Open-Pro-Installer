2c5462345c02b55d80f9f4fdd0d4d4e18be210ee
zimoadmin <141346171+zimoadmin@users.noreply.github.com>
2026-08-13 19:25:12 +0800

    Update github.sh

--- lib/github.sh
+++ lib/github.sh
@@ -8,15 +8,13 @@ get_latest_release() {
 
     wget -qO /tmp/openclash_version "$OPENCLASH_API"
 
-    if [ ! -f /tmp/openclash_version ]; then
-        error "Failed to download release information."
+    [ ! -f /tmp/openclash_version ] && {
+        error "Download release info failed"
         return 1
-    fi
+    }
 
-    # 获取版本号
     RELEASE_TAG="$(jsonfilter -i /tmp/openclash_version -e '@.tag_name')"
 
-    # 获取 IPK 下载地址
     DOWNLOAD_URL="$(jsonfilter -i /tmp/openclash_version -e '@.assets[*].browser_download_url' | grep '\.ipk$' | head -n1)"
 
     info "Latest Version : $RELEASE_TAG"
