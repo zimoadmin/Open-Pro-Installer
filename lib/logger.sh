dff07f471d88d24c50f29e88279e74fc80c320d8
zimoadmin <141346171+zimoadmin@users.noreply.github.com>
2026-08-14 14:50:35 +0800

    Update logger.sh

--- lib/logger.sh
+++ lib/logger.sh
@@ -2,9 +2,16 @@ info() {
     printf '\033[32m[INFO]\033[0m %s\n' "$*"
 }
 
+
 warn() {
     printf '\033[33m[WARN]\033[0m %s\n' "$*"
 }
+
+
+warning() {
+    warn "$@"
+}
+
 
 error() {
     printf '\033[31m[ERROR]\033[0m %s\n' "$*"
